8aa3cf8bcaaf6332e5da044abc29773e466f1522
Mhayk Whandson <hi@mhayk.com>
2022-07-05 14:14:10 +0100

    feat: Sidebar component has been styled and finished

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,18 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
+
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/mhayk.png"
+                />
                 <strong>Mhayk Whandson</strong>
-                <span>Web Developer</span>
+                <span>Software Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Edit Profile
                 </a>
             </footer>
