4a7529f1fee225fcafd252cc56d2beb634acc322
Mhayk Whandson <hi@mhayk.com>
2022-07-06 19:15:27 +0100

    feat: The component Avatar has been imported into the other components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,10 +12,7 @@ export function Sidebar() {
             />
             <div className={styles.profile}>
 
-                <img
-                    className={styles.avatar}
-                    src="https://github.com/mhayk.png"
-                />
+                <Avatar src="https://github.com/mhayk.png" />
                 <strong>Mhayk Whandson</strong>
                 <span>Software Developer</span>
             </div>
