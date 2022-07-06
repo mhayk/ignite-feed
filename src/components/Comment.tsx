86d8f3b551e6c3e7aa2c927ac9f0d10e19843086
Mhayk Whandson <hi@mhayk.com>
2022-07-06 09:56:40 +0100

    feat: Comment component structure has been created

---
+++ src/components/Comment.tsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/mhayk.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Mhayk Whandson</strong>
+                            <time title="5 Jul 2022" dateTime="">Around 1h ago</time>
+                        </div>
+
+                        <button title='Delete comment'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Very good Mhayk, congratulations!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Clap <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
